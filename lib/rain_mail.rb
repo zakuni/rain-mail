c7a4fe3621e5764d75929f9f884386040ed6970b
zakuni <kunio038@gmail.com>
2013-05-05 09:26:19 +0900

    use weather-report

--- lib/rain_mail.rb
+++ lib/rain_mail.rb
@@ -6,41 +6,19 @@ require 'rexml/document'
 require 'kconv'
 require 'net/smtp'
 
-class RainMail
-  def rainy?(weather)
-    return true if weather.include? "雨"
-  end
-
-  # 参考: http://d.hatena.ne.jp/yonetin/20120118/1326896786
-  # http://d.hatena.ne.jp/gan2/20070604/1180974366
-  def forecast_weather
-    uri = "http://weather.livedoor.com/forecast/webservice/rest/v1?city=70&day=tomorrow"
-    doc = nil
-    begin
-      open(uri) do |uri|
-        doc = REXML::Document.new(uri)
-      end
-      doc.elements["lwws"].elements["telop"].text
-    rescue SocketError
-      puts"SocketError:#{uri}"
-    rescue OpenURI::HTTPError
-      puts"HTTPError:#{uri}"
-    end
-  end
-
-  def send_mail(subject, body)
-    conf ||= YAML.load_file(File.dirname(__FILE__) + '/config.yaml')
-    smtp_server = conf['smtp_server']
-    smtp_port   = conf['smtp_port']
-    from_addr   = conf['from_addr']
-    to_addrs    = conf['to_addrs']
-    user        = conf['user']
-    pass        = conf['pass']
-
-    smtp = Net::SMTP.new(smtp_server, smtp_port)
-    smtp.enable_starttls
-    smtp.start('localhost.localdomain', user, pass, :plain) do |connection|
-      connection.send_mail(<<EOS , from_addr, *to_addrs)
+def send_mail(subject, body)
+  conf ||= YAML.load_file(File.dirname(__FILE__) + '/config.yaml')
+  smtp_server = conf['smtp_server']
+  smtp_port   = conf['smtp_port']
+  from_addr   = conf['from_addr']
+  to_addrs    = conf['to_addrs']
+  user        = conf['user']
+  pass        = conf['pass']
+  
+  smtp = Net::SMTP.new(smtp_server, smtp_port)
+  smtp.enable_starttls
+  smtp.start('localhost.localdomain', user, pass, :plain) do |connection|
+    connection.send_mail(<<EOS , from_addr, *to_addrs)
 Date: #{Time::now.strftime("%a, %d %b %Y %X")}
 From: #{from_addr}
 To: #{to_addrs.join(",")}
@@ -50,17 +28,16 @@ Content-Type: text/plain; charset=Shift_JIS
 
 #{body.encode("Shift_JIS")}
 EOS
-    end
   end
 end
 
-rain_mail = RainMail.new
-weather = rain_mail.forecast_weather
-subject_body = { "明日#{weather}らしいから" => "洗濯物干さないほうがよいよ。どちらでもよいよ。\nhttp://weather.livedoor.com/area/14/70.html",
-                 "明日いつから#{weather}なの?" => "明日の天気はもう、生まれたて。 \nhttp://weather.livedoor.com/area/14/70.html"}
+tomorrow = WeatherReport.get("横浜").tomorrow
 
-if rain_mail.rainy? weather then
+subject_body = { "明日#{tomorrow.telop}らしいから" => "洗濯物干さないほうがよいよ。どちらでもよいよ。\nhttp://weather.livedoor.com/area/14/70.html",
+                 "明日いつから#{tomorrow.telop}なの?" => "明日の天気はもう、生まれたて。 \nhttp://weather.livedoor.com/area/14/70.html" }
+
+if tomorrow.umbrella? then
   subject = subject_body.keys[rand(subject_body.length)]
   body = subject_body[subject]
-  rain_mail.send_mail(subject, body)
+  send_mail(subject, body)
 end
--- spec/rain_mail_spec.rb
+++ spec/rain_mail_spec.rb
@@ -1,27 +1,3 @@
 #!/usr/bin/env ruby
 # -*- coding: utf-8 -*-
 require 'rain_mail'
-
-describe RainMail do
-  before do
-    @rain_mail = RainMail.new
-  end
-  subject { @rain_mail }
-
-  describe '#rainy?' do
-    context "雨予報のとき" do
-      it 'はtrueであること' do
-        should be_rainy('雨')
-      end
-    end
-    context "晴れ予報のとき" do
-      it 'はfalseであること' do
-        should_not be_rainy('晴れ')
-      end
-    end
-  end
-
-  describe '#forecast_weather' do
-    its(:forecast_weather) { should_not be_nil }
-  end
-end
